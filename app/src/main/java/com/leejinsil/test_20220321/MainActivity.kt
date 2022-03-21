8b64003d054052a997b1b3fbf98d4f98ac771048
이진실 <ljsil1204@naver.com>
2022-03-21 16:05:40 +0900

    Initial commit

---
+++ app/src/main/java/com/leejinsil/test_20220321/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.leejinsil.test_20220321
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
